a314a5910a88af68a97af031d21e9efd36cb7d23
Ashran Haider <ashranhaider@hotmail.com>
2024-06-18 21:43:38 -0400

    layout implemented

--- CapstoneProject/Controllers/HomeController.cs
+++ CapstoneProject/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace CapstoneProject.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
